f8b8d825c023c249052241b5013f987c3c90fc76
Mats Hagen <mats.hagen2@gmail.com>
2023-03-15 19:27:34 +0100

    card component

---
+++ src/lib/components/card/Card.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import classNames from 'classnames';
+
+	export let header: string | null;
+	export let body: ConstructorOfATypedSvelteComponent | null = null;
+</script>
+
+<div class={classNames(`w-full max-w-3xl self-stretch rounded-lg bg-gray shadow-sm`)}>
+	<div class="flex items-center px-6 py-4">
+		<h2 class="text-base font-semibold">
+			{header}
+		</h2>
+	</div>
+	<div class="flex flex-col content-center gap-2 px-6 py-4">
+		{#if body}
+			<svelte:component this={body} />
+		{:else}
+			<slot />
+		{/if}
+	</div>
+</div>
--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import Card from '$lib/components/card/Card.svelte';
 	import Heading from '$lib/components/primitives/Heading.svelte';
 	import Text from '$lib/components/primitives/Text.svelte';
 	import type { PageData } from './$types';
@@ -11,14 +12,29 @@
 </script>
 
 <div class="grid w-full grid-cols-6">
-	<div class="col-span-4 flex py-4">
-		<div class="flex flex-col">
-			<Heading customClass="text-5xl">{user?.firstName} {user?.lastName}</Heading>
+	<div class="col-span-4 flex w-full">
+		<div class="grid flex-1 grid-rows-6">
+			<Heading customClass="text-5xl row-span-2">{user?.firstName} {user?.lastName}</Heading>
 
-			<div class="flex flex-col">
-				<Text>Email: {user?.email}</Text>
+			<div class="row-span-4 flex flex-col">
+				<Card header="Konto">
+					<div class="text-row">
+						<Text>Email:</Text>
+						<Text type="strong">{user?.email}</Text>
+					</div>
+					<div class="text-row">
+						<Text>Phone:</Text>
+						<Text type="strong">{user?.phone}</Text>
+					</div>
+				</Card>
 			</div>
 		</div>
 	</div>
 	<div class="col-span-2 flex" />
 </div>
+
+<style lang="scss">
+	.text-row {
+		@apply flex items-baseline gap-32;
+	}
+</style>
