7b0a0422090e838ae772da7baf0c053541dee6c6
Mats Hagen <mats.hagen2@gmail.com>
2023-07-04 09:32:34 +0200

    remove unused route

--- src/routes/api/refresh/+server.ts
+++
@@ -1,8 +0,0 @@
-import { refresh } from '$lib/hooks/auth';
-import { json } from '@sveltejs/kit';
-import type { RequestHandler } from '../auth/signin/$types';
-
-export const GET = (async () => {
-	const refreshed = await refresh();
-	return json(refreshed);
-}) satisfies RequestHandler;
